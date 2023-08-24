f9b22fff27dd28a9d1887d35e4d33bb759a77722
gdna <salleh.fian24@gmail.com>
2023-08-24 10:26:06 +0200

    app.js

--- app.js
+++ app.js
@@ -14,12 +14,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname + 'views/beers.hbs'));
+
+
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get("/home", (req, res, next) => {res.render("index", data)})
+
+app.get("/beers", (req, res, next) => {
+  let data = {}
+  const beers= punkAPI.getBeers(25)
+  beers
+    .then((beers) => { 
+      data = {
+        beers: {...beers}
+      }
+      console.log(data.beers)
+  })
+    .catch((err)=>console.log("catch", err))
+  
+  res.render("beers", data)
+  
+  
+})
+
+app.get("/randomBeers", (req, res, next) => {res.render("randomBeer", data)})
+
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
